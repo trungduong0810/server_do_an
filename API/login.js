0adf3ed6720d374c3341febaeb9350431539c645
trungduong0810 <141097665+trungduong0810@users.noreply.github.com>
2024-12-10 00:45:56 +0700

    Update login.js

--- API/login.js
+++ API/login.js
@@ -32,7 +32,7 @@ apiLoginAccount.post("/api/login", async (req, res) => {
     );
     res.cookie("accessToken", accessToken, {
       maxAge: 24 * 60 * 60 * 1000, // Thời gian sống 1 ngày (ms)
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       path: '/',
       sameSite: 'None',
